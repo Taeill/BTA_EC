9462042fbe616ee50fdd8059da103e4e6e4ef2de
Ellie Gandrille <gandrille.t@mail.com>
2023-08-25 16:37:19 +0200

    Update Enemy Anim Punch + WIP Knockback

--- Assets/Project/Script/Enemy/Enemy_Movement.cs
+++ Assets/Project/Script/Enemy/Enemy_Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Enemy_Movement : MonoBehaviour
 {
@@ -15,19 +16,20 @@ public class Enemy_Movement : MonoBehaviour
     private Vector2 _startPos;
 
 
-
-    Vector2 _playerVelocity
+    //Constructor
+    Vector2 _enemyVelocity
     {
         get { return _rgbd2d.velocity; }
         set
         {
             _rgbd2d.velocity = value;
             _Animator.SetFloat("Velocity", value.magnitude);
+            
             if (value.x >= 0.1) 
             {
                 transform.root.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));  
             }
-            else if (value.x <= 0.1)
+            else if (value.x <= -0.1)
             {
                 transform.root.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
             }
@@ -37,12 +39,18 @@ public class Enemy_Movement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.transform.gameObject.GetComponent<PlayerMovement>() != null)
+        {
+            _playerInRange = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.transform.gameObject.GetComponent<PlayerMovement>() != null)
+        {
+            _playerInRange = false;
+        }
     }
 
     private void Start()
@@ -54,7 +62,17 @@ public class Enemy_Movement : MonoBehaviour
     {
         if (_playerInRange) // Chase And Fight Behavior
         {
+            
 
+            if(Vector3.Distance(transform.position, PlayerMovement.Instance.transform.position) <= 1)
+            {
+                _Animator.SetTrigger("Attack");
+                _enemyVelocity = Vector3.zero;
+            }
+            else
+            {
+                _enemyVelocity = (new Vector3(PlayerMovement.Instance.transform.position.x, PlayerMovement.Instance.transform.position.y, 0) - transform.position).normalized;
+            }
         }
         else if (!_playerInRange) //Idle Routine
         {
@@ -66,7 +84,7 @@ public class Enemy_Movement : MonoBehaviour
             }
             if (Vector3.Distance(transform.position, _currentGoal) <= 1 && !_currentGoalDone)
             {
-                _playerVelocity = Vector3.zero;
+                _enemyVelocity = Vector3.zero;
                 StartCoroutine(RandomWaitBetweenWalk());
                 _currentGoalDone = true;
             }
@@ -83,12 +101,17 @@ public class Enemy_Movement : MonoBehaviour
 
     void GoTo(Vector2 a)
     {
-        _playerVelocity = (new Vector3 (a.x,a.y,0) - transform.position).normalized;
+        _enemyVelocity = (new Vector3 (a.x,a.y,0) - transform.position).normalized;
     }
 
     IEnumerator RandomWaitBetweenWalk()
     {
         yield return new WaitForSeconds(Random.Range(1,5));
         _currentWaitDone = true;
+    }
+
+    void TakeDamage()
+    {
+
     }
 }
---
+++ Assets/Project/Script/Reaction/CharacterKnockback.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterKnockback : MonoBehaviour
+{
+
+    void Knockback(GameObject a, Rigidbody2D b)
+    {
+        //a.AddForce (new Vector3 ())
+    }
+
+}
