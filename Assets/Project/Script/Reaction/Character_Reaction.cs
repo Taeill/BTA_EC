c3befe8b40f0606dbf136efbeffd553a9a32ecdb
Ellie Gandrille <gandrille.t@mail.com>
2023-08-28 17:21:13 +0200

    minor fix

--- Assets/Project/Script/Reaction/Character_Reaction.cs
+++ Assets/Project/Script/Reaction/Character_Reaction.cs
@@ -7,7 +7,7 @@ public class Character_Reaction : MonoBehaviour
     [SerializeField] Material _blinkMat;
     public void Knockback(GameObject target, GameObject sender)
     {
-        StartCoroutine(Knockbacking(target, sender));
+        StartCoroutine(Knockbacking(target, sender));        
     }
 
     public void Blinking(GameObject a)
@@ -38,12 +38,11 @@ public class Character_Reaction : MonoBehaviour
 
     IEnumerator Knockbacking(GameObject target, GameObject sender)
     {
-        Rigidbody2D currentRgbd = target.GetComponent<Rigidbody2D>();
-        currentRgbd.bodyType = RigidbodyType2D.Dynamic;
-        currentRgbd.AddForce(new Vector2((target.transform.position.x - sender.transform.position.x), 0).normalized * 300);
-        yield return new WaitForSeconds(0.1f);
-        currentRgbd.bodyType = RigidbodyType2D.Kinematic;
-        currentRgbd.velocity = Vector2.zero;
-        
+            Rigidbody2D currentRgbd = target.GetComponent<Rigidbody2D>();
+            currentRgbd.bodyType = RigidbodyType2D.Dynamic;
+            currentRgbd.AddForce(new Vector2((target.transform.position.x - sender.transform.position.x), 0).normalized * 300);
+            yield return new WaitForSeconds(0.1f);
+            currentRgbd.bodyType = RigidbodyType2D.Kinematic;
+            currentRgbd.velocity = Vector2.zero;
     }   
 }
