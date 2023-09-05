6449113c3b92fc072bb98b1881afb3e304b7e7f0
Ellie Gandrille <gandrille.t@mail.com>
2023-09-05 09:37:39 +0200

    fix enemy

--- Assets/Project/Script/Enemy/Enemy_Behavior.cs
+++ Assets/Project/Script/Enemy/Enemy_Behavior.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy_Behavior : MonoBehaviour
 {
+    [SerializeField] Animator _animator;
+    [SerializeField] AnimatorOverrideController _animator2;
     BossBehavior _owner;
 
     int _ownerListIndex;
@@ -13,4 +15,15 @@ public class Enemy_Behavior : MonoBehaviour
     public BossBehavior Owner { get => _owner; set => _owner = value; }
     public int OwnerListIndex { get => _ownerListIndex; set => _ownerListIndex = value; }
     public bool BossSpawned { get => _bossSpawned; set => _bossSpawned = value; }
+
+    private void Start()
+    {
+        switch(Random.Range(0, 2))
+        {
+            case 1:
+                _animator.runtimeAnimatorController = _animator2;
+                break;
+        }
+    }
 }
+
